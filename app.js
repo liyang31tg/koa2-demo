d6915cbcacb74df898c065c513bb2e2e7ab0a6e6
离子 <liyang20097@163.com>
2016-08-24 16:56:05 +0800

    加入koa-static

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 /**
  * Created by liyang on 16/8/24.
  */
-const Koa = require('koa')
-const app = new Koa()
+const Koa           = require('koa')
+const app           = new Koa()
+const convert       = require('koa-convert')
+const staticserver  = require("koa-static")
+
+//开启public的静态服务
+console.log(__dirname);
+app.use(convert(staticserver(__dirname + "/public")))
 
 // response
 app.use(ctx => {
