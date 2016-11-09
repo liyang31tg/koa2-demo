ca7fed1a90c46aeadfb73fab36e744034725b4d8
离子 <liyang20097@163.com>
2016-11-09 17:40:20 +0800

    修复runkoa找不到入口文件问题

--- bin/start.js
+++ bin/start.js
@@ -4,4 +4,4 @@
 const current_path = process.cwd();
 const runkoa = require('runkoa')
 const path = require('path')
-runkoa(path.join(current_path,'index'))
+runkoa(path.join(current_path,'index.js'))//mark不能省略后缀.js
